f0833a58a6d84d368c0c412013ddc6ba898b1676
RoxanaAnamariaTurc <roxi.anamaria25@gmail.com>
2022-08-01 20:10:13 +0100

    Created Robots webpage

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import CardList from './CardList';
+// import Card from './Card';
 import './index.css';
-import App from './App';
+import App from './Containers/App';
 import reportWebVitals from './reportWebVitals';
+import tachyons from 'tachyons';
+// import { robots } from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
